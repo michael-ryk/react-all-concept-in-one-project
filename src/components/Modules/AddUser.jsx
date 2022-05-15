7900e32baebbda659cb23d9427c3caed05a451c5
MichaelR <spider747@gmail.com>
2022-05-15 22:00:12 +0300

    Add useRef to input fields

--- src/components/Modules/AddUser.jsx
+++ src/components/Modules/AddUser.jsx
@@ -1,19 +1,18 @@
-import {useState} from 'react';
+import { useState, useRef } from 'react';
 import Error from '../UI/Error';
 
-function AddUser(props){
-
-  const [user, setUser] = useState({name: '', age: ''})
+function AddUser(props) {
+  const nameRef = useRef();
+  const ageRef = useRef();
+  const [user, setUser] = useState({ name: '', age: '' });
   const [error, setError] = useState();
 
-  console.log(error)
-
-  function handleChange(e){
-    const {name, value} = e.target;
+  function handleChange(e) {
+    const { name, value } = e.target;
     setError(null);
     setUser((prev) => {
-      return {...prev, [name]:value}
-    })
+      return { ...prev, [name]: value };
+    });
   }
 
   function submitNewUser(e) {
@@ -21,31 +20,43 @@ function AddUser(props){
     
     // Validate incorrect values
     if (user.name.trim().length === 0 || user.age.trim().length === 0) {
-      setError({title: 'No input detected', message: 'Dont leave inputs empty'})
+      setError({
+        title: 'No input detected',
+        message: 'Dont leave inputs empty',
+      });
       return;
-    };
+    }
     if (user.age < 1) {
-      setError({title: 'Age is too low', message: 'Enter Valid Age!!'})
+      setError({ title: 'Age is too low', message: 'Enter Valid Age!!' });
       return;
-    };
+    }
 
-    const newUserWithId = {id: new Date().getTime().toString() , ...user}
-    props.addUser(newUserWithId)
+    const newUserWithId = { id: new Date().getTime().toString(), ...user };
+    props.addUser(newUserWithId);
   }
 
   return (
     <>
-    {error && <Error object={error}/>}
+      {error && <Error object={error} />}
       <form onSubmit={submitNewUser}>
         <label>User Name: </label>
-        <input name='name' onChange={handleChange} value={user.name}></input>
+        <input
+          name='name'
+          onChange={handleChange}
+          value={user.name}
+          ref={nameRef}
+        ></input>
         <label>Age: </label>
-        <input name='age' onChange={handleChange} value={user.age}></input>
+        <input
+          name='age'
+          onChange={handleChange}
+          value={user.age}
+          ref={ageRef}
+        ></input>
         <button type='submit'>Add User</button>
       </form>
     </>
   );
-
 }
 
 export default AddUser;
