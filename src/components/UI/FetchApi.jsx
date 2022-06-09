2f350cb58218aaee4cf03fe7c45fcb3e72356328
MichaelR <spider747@gmail.com>
2022-06-09 21:05:29 +0300

    Upgrade fetch function using async await

--- src/components/UI/FetchApi.jsx
+++ src/components/UI/FetchApi.jsx
@@ -4,15 +4,38 @@ import dummyMovies from '../FetchApi/dummyMovies';
 import MovieList from '../FetchApi/MoviesList';
 
 const FetchApi = () => {
+
+  //State to save fetched data
   const [movies, setMovies] = useState();
 
-  //function to fetch data from backend
-  function fetchMoviesHandler() {
+  //function to fetch data from backend using Async Wait
+  async function fetchMoviesHandler() {
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+        
+    //Transfrom received object to have properties that component expect
+    const transformedArray = data.results.map(item => {
+      return {
+        id: item.episode_id,
+        title: item.title,
+        openingText: item.opening_crawl,
+        releaseDate: item.release_date
+      }
+    });
+
+    //Put transformed array to state
+    setMovies(transformedArray);
+
+  }
+
+  // Example of fetch without Async Wait - we don't use it
+  function fetchMoviesWithoutAsyncWait() {
     fetch('https://swapi.dev/api/films/')
       .then((response) => {
         return response.json();
       })
       .then((data) => {
+        
         //Transfrom received object to have properties that component expect
         const transformedArray = data.results.map(item => {
           return {
@@ -23,9 +46,9 @@ const FetchApi = () => {
           }
         });
 
+        //Put transformed array to state
         setMovies(transformedArray);
       });
-
   }
 
   return (
