acb5c34db88144637f3c715aa9e8c0d62456f960
MichaelR <spider747@gmail.com>
2022-08-06 20:30:30 +0300

    Add typescript pacakge to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import UserModule from './components/UI/UserModule'
 import FetchApi from './components/UI/FetchApi';
+import TypeScript from './components/TypeScript';
 
 function App() {
   return (
     <div className="App">
+      <TypeScript />
       <UserModule />
       <FetchApi />
     </div>
---
+++ src/components/TypeScript.jsx
@@ -0,0 +1,19 @@
+const TypeScript = () => {
+
+const add = (a, b) => {
+  return a+b;
+}
+
+return (
+  <div>
+    <h1>TypeScript</h1>
+    <input  />
+    <input  />
+    <button>Calculate</button>
+    <h3>Result here</h3>
+  </div>
+)
+
+}
+
+export default TypeScript;
