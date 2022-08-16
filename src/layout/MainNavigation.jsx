11ddbad1da010ab24487c07999b152f924a69f0a
MichaelR <spider747@gmail.com>
2022-08-16 20:01:06 +0300

    Create links to pages

--- src/layout/MainNavigation.jsx
+++ src/layout/MainNavigation.jsx
@@ -2,18 +2,25 @@ import { NavLink } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
+  let activeClassName = classes.active;
   return (
     <header className={classes['main-header']} >
-      <nav >
+      <nav className={classes.nav}>
         <ul>
           <li>
-            Link1
+            <NavLink to='homepage' className={({isActive}) => isActive ? activeClassName : undefined}>
+              HomePage
+            </NavLink>
           </li>
           <li>
-            Link2
+            <NavLink to='usermodule' className={({isActive}) => isActive ? activeClassName : undefined}>
+              Users
+            </NavLink>
           </li>
           <li>
-            Link3
+            <NavLink to='fetchapi' className={({isActive}) => isActive ? activeClassName : undefined}>
+              Fetch
+            </NavLink>
           </li>
         </ul>
       </nav>
