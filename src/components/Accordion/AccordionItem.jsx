acbc1010707a220fc44c869c258f9d4aec6a5d5f
MichaelR <spider747@gmail.com>
2022-08-23 14:12:41 +0300

    Add Accordion functionality

--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,14 +1,21 @@
+import { useState } from 'react';
+
 import classes from './AccordionItem.module.css';
 const AccordionItem = ({question, answer}) => {
 
+  const [ answerShown, setAnswerShown] = useState(false);
+
+  const toggleAnswer = () => {
+    setAnswerShown(prev => !prev);
+  }
 
   return (
     <article className={classes.acccontainer}>
       <header className={classes.accheader}>
         <h4>{question}</h4>
-        <button className={classes.btn}>Expand</button>
+        <button className={classes.btn} onClick={toggleAnswer}>{answerShown ? 'Collapse' : 'Expand'}</button>
       </header>
-      <p>{answer}</p>
+      {answerShown && <p>{answer}</p>}
     </article>
   )
 }
