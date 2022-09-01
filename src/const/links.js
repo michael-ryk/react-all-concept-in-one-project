b9d53a55172e7b83c8134bdb4103dffd50f3301a
MichaelR <spider747@gmail.com>
2022-09-01 19:59:28 +0300

    Move links to external file

---
+++ src/const/links.js
@@ -0,0 +1,27 @@
+export const links = [
+  {
+    id: 1,
+    url: '/homepage',
+    text: 'HomePage',
+  },
+  {
+    id: 2,
+    url: '/add-user',
+    text: 'Users',
+  },
+  {
+    id: 3,
+    url: '/fetch-movies',
+    text: 'Fetch',
+  },
+  {
+    id: 4,
+    url: '/accordion',
+    text: 'Accordion',
+  },
+  {
+    id: 5,
+    url: '/products-filter',
+    text: 'Products Filter',
+  },
+];
