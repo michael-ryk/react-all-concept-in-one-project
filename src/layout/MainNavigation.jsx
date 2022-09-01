0e795a64b703b3858dbb3a7121c31bd4638221a6
MichaelR <spider747@gmail.com>
2022-09-01 20:04:25 +0300

    Replace hardcoded links with map other links array

--- src/layout/MainNavigation.jsx
+++ src/layout/MainNavigation.jsx
@@ -1,4 +1,7 @@
 import { NavLink } from 'react-router-dom';
+
+import { links } from '../const/links';
+
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
@@ -7,31 +10,15 @@ const MainNavigation = () => {
     <header className={classes['main-header']} >
       <nav className={classes.nav}>
         <ul>
-          <li>
-            <NavLink to='homepage' className={({isActive}) => isActive ? activeClassName : undefined}>
-              HomePage
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='add-user' className={({isActive}) => isActive ? activeClassName : undefined}>
-              Users
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='fetch-movies' className={({isActive}) => isActive ? activeClassName : undefined}>
-              Fetch
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='accordion' className={({isActive}) => isActive ? activeClassName : undefined}>
-              Accordion
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='products-filter' className={({isActive}) => isActive ? activeClassName : undefined}>
-              Products Filter
-            </NavLink>
-          </li>
+          {links.map( link => {
+            return (
+              <li key={link.id}>
+                <NavLink to={link.url} className={({isActive}) => isActive ? activeClassName : undefined}>
+                  {link.text}
+                </NavLink>    
+              </li>
+            )
+          })}
         </ul>
       </nav>
     </header>
